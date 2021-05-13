4e3c6505c03f9a2a61e1e88f515238b5ec2fae7e
glukvit <glukvit@gmail.com>
2021-05-13 14:42:47 +1200

    remove_trash

--- join_csv_file.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd    
-from pathlib import Path
-
-data_dir = Path("/media/gluk/ca1c79c0-ddfe-428e-8c1a-b57d5bbad96c/EO_CONVERT/ASC/min")
-
-df = pd.concat([pd.read_csv(f, compression='gzip') for f in data_dir.glob("*.csv")], ignore_index=True)
-df.to_csv("/media/gluk/ca1c79c0-ddfe-428e-8c1a-b57d5bbad96c/EO_CONVERT/ASC/all_1min.csv", index=False)
